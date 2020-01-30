830da0530d8c1e9a9452e25d4a6ff635c8d1df8d
maradiuh <maradug13@gmail.com>
2020-01-30 23:04:26 +0200

    Adding, editing reports. Notifying. No validation

--- src/app/components/main-response/reports/reports.component.ts
+++ src/app/components/main-response/reports/reports.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import { Time } from "@angular/common";
+import { Time, DatePipe } from "@angular/common";
 import { IReportData } from "src/app/interfaces/report-data";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { DateAdapter, MatDatepicker } from "@angular/material";
+import * as moment from "moment";
 
 @Component({
   selector: "app-reports",
@@ -9,14 +12,50 @@ import { IReportData } from "src/app/interfaces/report-data";
 })
 export class ReportsComponent implements OnInit {
   projects: string[] = ["EDU-pr", "MED", "cancerheal", "adasdad"];
-  tasks: string[] = ['bug fixing', 'testing', 'dev', 'design'];
-  statuses: string[] = ["Opened","Notified", "Accepted", "Declined"];
+  tasks: string[] = ["bug fixing", "testing", "dev", "design"];
+  statuses: string[] = ["Opened", "Notified", "Accepted", "Declined"];
 
   reports: Array<IReportData> = new Array<IReportData>();
+  reportForm: FormGroup;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.reportForm = new FormGroup({
+      projectControl: new FormControl(null, Validators.required),
+      taskControl: new FormControl(null, Validators.required),
+      timeControl: new FormControl(null, Validators.required),
+      overtimeControl: new FormControl(null, Validators.required),
+      descriptionControl: new FormControl(null, Validators.required),
+      startDateControl: new FormControl(null, Validators.required),
+      endDateControl: new FormControl(null, Validators.required),
+      statusControl: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    const timeDate = this.timeToDate(this.reportForm.get("timeControl").value);
+    const overtimeDate = this.timeToDate(
+      this.reportForm.get("overtimeControl").value
+    );
+
+    const reportData: IReportData = {
+      project: this.reportForm.get("projectControl").value,
+      task: this.reportForm.get("taskControl").value,
+      time: { hours: timeDate.hours(), minutes: timeDate.minutes() },
+      overtime: {
+        hours: overtimeDate.hours(),
+        minutes: overtimeDate.minutes()
+      },
+      description: this.reportForm.get("descriptionControl").value,
+      startDate: this.reportForm.get("startDateControl").value,
+      endDate: this.reportForm.get("endDateControl").value,
+      status: "opened"
+    };
+
+    this.reports.push(reportData);
+    this.reportForm.reset();
+  }
 
   onCreateReportTemplate() {
     const report: IReportData = {
@@ -28,8 +67,42 @@ export class ReportsComponent implements OnInit {
       description: "Creating a report",
       startDate: "13.01.2020",
       endDate: "14.01.2020",
-      status: "opened"
+      status: "approved"
     };
     this.reports.push(report);
+  }
+
+  timeToDate(time: string) {
+    time = time === null ? "00:00" : time;
+    const date = "2020-02-08";
+    const today = moment(date + " " + time);
+    return today;
+  }
+
+  onEditReport(report: IReportData) {
+    this.reportForm.reset();
+    const time: string = report.time.hours + ":" + report.time.minutes;
+    const overtime: string =
+      report.overtime.hours + ":" + report.overtime.minutes;
+
+    this.reportForm.patchValue({ projectControl: report.project });
+    this.reportForm.patchValue({ taskControl: report.task });
+    this.reportForm.patchValue({ timeControl: time });
+    this.reportForm.patchValue({ overtimeControl: overtime });
+    this.reportForm.patchValue({ descriptionControl: report.description });
+    this.reportForm.patchValue({ startDateControl: report.startDate });
+    this.reportForm.patchValue({ endDateControl: report.endDate });
+
+    const index: number = this.reports.indexOf(report);
+    if (index !== -1) {
+      this.reports.splice(index, 1);
+    }
+  }
+
+  onNotify(report: IReportData) {
+    const index: number = this.reports.indexOf(report);
+    if (index !== -1) {
+      this.reports[index].status="notified";
+    } 
   }
 }
