714364d14ce0d904b1e4dabdf539a5f1f4727aab
Kamikotomushoo <58001338+Kamikotomushoo@users.noreply.github.com>
2020-02-07 00:13:35 +0200

    fixed startDate validator

--- src/app/validators/reports.validator.ts
+++ src/app/validators/reports.validator.ts
@@ -5,7 +5,7 @@ export class ReportValidator {
   startDateValidation(startDateInput: FormControl): { [s: string]: boolean } {
     let currentDate = new Date(new Date().toDateString());
 
-    if (startDateInput.value !== null) {
+    if (startDateInput.value !== null && startDateInput !== undefined) {
       let inputDate = new Date(new Date(startDateInput.value).toDateString());
       if (currentDate > inputDate || currentDate.toDateString() === inputDate.toDateString() ) {
         return null;
