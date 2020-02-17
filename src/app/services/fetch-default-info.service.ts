96bfd9109e69ffe966b6ad4459dc0cfbbd123517
Kamikotomushoo <58001338+Kamikotomushoo@users.noreply.github.com>
2020-02-18 00:52:05 +0200

    fetch data service

---
+++ src/app/services/fetch-default-info.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { IAssignment, IPosition, IStatus } from "../interfaces/report-data";
+import { ErrorResponseDialogComponent } from "../components/main-response/error-response-dialog/error-response-dialog.component";
+import { MatDialog } from "@angular/material";
+
+interface IDefaultInfo {
+  assignmentDto: Array<IAssignment>;
+  possitions: Array<IPosition>;
+  statusDto: Array<IStatus>;
+}
+
+@Injectable()
+export class FetchDefaultInfoService {
+  constructor(private http: HttpClient, private dialog: MatDialog) {}
+
+  fetchDefaultInfo() {
+    this.http
+      .get<IDefaultInfo>(environment.apiUrl + "/info")
+      .pipe(catchError(this.errorHandling))
+      .subscribe(
+        responseData => {
+          localStorage.setItem("tasksData", JSON.stringify(responseData.assignmentDto));
+          localStorage.setItem("possitionsData", JSON.stringify(responseData.possitions));
+          localStorage.setItem("statusesData", JSON.stringify(responseData.statusDto));
+        },
+        errorData => {
+          this.openErrorResponseDialog(errorData);
+        }
+      );
+  }
+
+
+
+  private errorHandling(errorResponse: HttpErrorResponse) {
+    return throwError(errorResponse.message);
+  }
+
+  private openErrorResponseDialog(errorName: string) {
+    const dialogRef = this.dialog.open(ErrorResponseDialogComponent, {
+      width: "fit-content",
+      data: errorName
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("The dialog was closed");
+    });
+  }
+}
