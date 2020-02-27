cf8e185ab759e6ca4a888cedf758e4913c8f4f6c
Kamikotomushoo <58001338+Kamikotomushoo@users.noreply.github.com>
2020-02-27 19:25:16 +0200

    some upgrade on home page

--- src/app/components/main-response/home/home.component.ts
+++ src/app/components/main-response/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { SignInUpService } from "src/app/services/sign-in-up.service";
 import { IPosition } from "src/app/interfaces/report-data";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 import { FetchDefaultInfoService } from "src/app/services/fetch-default-info.service";
 import { ErrorResponseDialogComponent } from "../error-response-dialog/error-response-dialog.component";
 import { MatDialog } from "@angular/material";
@@ -13,20 +13,33 @@ import { MatDialog } from "@angular/material";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
   positionId: number;
   positions: Array<IPosition> = [];
+  name: string;
+  userEmail: string;
 
   constructor(
     private signInUp: SignInUpService,
     private defaultInfoService: FetchDefaultInfoService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-
     this.positionId = +JSON.parse(localStorage.getItem("userData")).positionId;
     console.log(this.positionId);
+    switch (this.positionId) {
+      case 1:
+        this.router.navigate(["reportslist"], { relativeTo: this.route });
+        break;
+      case 2:
+        this.router.navigate(["reportslist"], { relativeTo: this.route });
+        break;
+      case 3:
+        this.router.navigate(["projectslist"], { relativeTo: this.route });
+        break;
+    }
     this.defaultInfoService.fetchDefaultInfo().subscribe(
       responseData => {
         console.log(responseData);
@@ -48,6 +61,11 @@ export class HomeComponent implements OnInit {
         this.openErrorResponseDialog(errorData);
       }
     );
+    this.name =
+      JSON.parse(localStorage.getItem("userData")).firstName +
+      " " +
+      JSON.parse(localStorage.getItem("userData")).secondName;
+      this.userEmail = JSON.parse(localStorage.getItem("userData")).email;
   }
 
   onLogout() {
